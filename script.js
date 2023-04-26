c2a9d32497d90bc8044cfde3ba38bd82b51bad58
Christian <christiandavid6734@gmail.com>
2023-04-26 11:30:35 -0300

    Sincroniza arquivos.

---
+++ script.js
@@ -0,0 +1,13 @@
+function Verificar() {
+    let nome = document.getElementById('nome').value;
+    let telefone = document.getElementById('telefone').value;
+    let nascimento = document.getElementById('nascimento').value;
+    let cpf = document.getElementById('cpf').value;
+    let endereco = document.getElementById('endereco').value;
+    let email = document.getElementById('email').value;
+    let senha = document.getElementById('senha').value;
+    let csenha = document.getElementById('csenha').value;
+
+    if(!nome || !telefone || !nascimento || !cpf || !email || !endereco || !senha || !csenha)
+    alert("Campos de preenchimento obrigatório vazio!");
+}
